bb27b9af789b47ace7f7fe3d84d6457aad4d714e
Promise Orung <magnificentorung@gmail.com>
2023-08-25 03:00:53 +0100

    improve image

--- constant/data.js
+++ constant/data.js
@@ -1,26 +1,41 @@
 // import React from 'react';
+import diabetics from '../src/assets/images/diabetes.png'
+import emphatic from '../src/assets/images/emphatic.png'
+import drip from '../src/assets/images/drip.png'
+import orak from '../src/assets/images/orak.png'
+import energy from '../src/assets/images/energy2.png'
+import energy_2 from '../src/assets/images/energy3.png'
+import energy_3 from '../src/assets/images/energy5.png'
+import energy_4 from '../src/assets/images/energy4.png'
+import injury from '../src/assets/images/injury.png'
+import energy_1 from '../src/assets/images/energy1.png'
+import respiratory from '../src/assets/images/respiratory.png'
+import sti from '../src/assets/images/sti.png'
+import pain from '../src/assets/images/pain.png'
+
+
 
 export const PainRelief = [
-    { id: "1", Image: "/src/assets/Images/Diabetes.png", drug: "Diabetes Regulator Powder", price: "3000", },
-    { id: "2", Image: "/src/assets/Images/Emphatic.png", drug: "Diabetes Regulator Powder", price: "3000", },
-    { id: "3", Image: "/src/assets/Images/drip.png", drug: "Diabetes Regulator Powder", price: "3000", },
-    { id: "4", Image: "/src/assets/Images/Orak.png", drug: "Diabetes Regulator Powder", price: "3000", },
+    { id: "1", Image: diabetics, drug: "Diabetes Regulator Powder", price: "3000", },
+    { id: "2", Image: emphatic, drug: "Diabetes Regulator Powder", price: "3000", },
+    { id: "3", Image: drip, drug: "Diabetes Regulator Powder", price: "3000", },
+    { id: "4", Image: orak, drug: "Diabetes Regulator Powder", price: "3000", },
 
 ];
 
 export const EnergyBooster = [
-    { id: "1", Image: "/src/assets/Images/Energy2.png", drug: "Diabetes Regulator Powder", price: "3000", },
-    { id: "2", Image: "/src/assets/Images/Energy3.png", drug: "Diabetes Regulator Powder", price: "3000", },
-    { id: "3", Image: "/src/assets/Images/Energy5.png", drug: "Diabetes Regulator Powder", price: "3000", },
-    { id: "4", Image: "/src/assets/Images/Energy4.png", drug: "Diabetes Regulator Powder", price: "3000", },
+    { id: "1", Image: energy, drug: "Diabetes Regulator Powder", price: "3000", },
+    { id: "2", Image: energy_2, drug: "Diabetes Regulator Powder", price: "3000", },
+    { id: "3", Image: energy_3, drug: "Diabetes Regulator Powder", price: "3000", },
+    { id: "4", Image: energy_4, drug: "Diabetes Regulator Powder", price: "3000", },
 
 ];
 
 export const Categories = [
-    { id: "1", Image: "/src/assets/Images/injury.png", treatment: "injury", },
-    { id: "2", Image: "/src/assets/Images/Energy1.png", treatment: "Energy Booster", },
-    { id: "3", Image: "/src/assets/Images/respiratory.png", treatment: "Respiratory disease", },
-    { id: "4", Image: "/src/assets/Images/STI.png", treatment: "Sexually Transmitted Infections", },
-    { id: "4", Image: "/src/assets/Images/Pain.png", treatment: "Pain Reliefs", },
+    { id: "1", Image: injury, treatment: "injury" },
+    { id: "2", Image: energy_1, treatment: "Energy Booster" },
+    { id: "3", Image:respiratory , treatment: "Respiratory disease" },
+    { id: "4", Image: sti, treatment: "Sexually Transmitted Infections" },
+    { id: "5", Image: pain, treatment: "Pain Reliefs" }
 
 ];
