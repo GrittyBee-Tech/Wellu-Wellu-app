ab47f6782facbf94856b7b9fe1e2f1081227eb29
c_orungpro <promise.orung@ng.ab-inbev.com>
2023-08-26 08:46:03 +0100

    add imag

--- src/components/Home/Products.jsx
+++ src/components/Home/Products.jsx
@@ -4,10 +4,10 @@ import stars from "../../assets/Icons/stars.png";
 import drip from '../../assets/img/drip.png'
 import orak from '../../assets/img/oral.png'
 import energy from '../../assets/img/respiratory.png'
-import energy_2 from '../../assets/img/pain.png'
-import energy_3 from '../../assets/img/pain.png'
-import energy_4 from '../../assets/img/pain.png'
-// import injury from '../assets/img/injury.png'
+// import energy_2 from '../../assets/img/pain.png'
+// import energy_3 from '../../assets/img/pain.png'
+// import energy_4 from '../../assets/img/pain.png'
+// // import injury from '../assets/img/injury.png'
 // import energy_1 from '../assets/img/energy1.png'
 // import respiratory from '../../assets/img/respiratory.png'
 // import sti from '../assets/img/sti.png'
@@ -25,9 +25,9 @@ import energy_4 from '../../assets/img/pain.png'
 
   const EnergyBooster = [
     { id: "1", Image: energy, drug: "Diabetes Regulator Powder", price: "3000", },
-    { id: "2", Image: energy_2, drug: "Diabetes Regulator Powder", price: "3000", },
-    { id: "3", Image: energy_3, drug: "Diabetes Regulator Powder", price: "3000", },
-    { id: "4", Image: energy_4, drug: "Diabetes Regulator Powder", price: "3000", },
+    { id: "2", Image: energy, drug: "Diabetes Regulator Powder", price: "3000", },
+    { id: "3", Image: energy, drug: "Diabetes Regulator Powder", price: "3000", },
+    { id: "4", Image: energy, drug: "Diabetes Regulator Powder", price: "3000", },
 
 ];
  
